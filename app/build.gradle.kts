6969c5d00931a787142133b504addbceed8faf4b
BarbedRuff <Barbed.Ruff@yandex.ru>
2024-04-18 16:59:14 +0300

    (Update): Update gradle implementation and .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            resValue("string", "apiKey", apikeyProperties["API_KEY"].toString())
-            resValue("string", "apiHost", apikeyProperties["API_HOST"].toString())
+            resValue("string", "token", apikeyProperties["token"].toString())
         }
         debug{
-            resValue("string", "apiKey", apikeyProperties["API_KEY"].toString())
-            resValue("string", "apiHost", apikeyProperties["API_HOST"].toString())
+            resValue("string", "token", apikeyProperties["token"].toString())
         }
     }
     compileOptions {
@@ -65,24 +63,25 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:latest")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    runtimeOnly("androidx.sqlite:sqlite-ktx:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.4")
 }
